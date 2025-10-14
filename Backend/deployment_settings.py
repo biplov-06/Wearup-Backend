41c0fa30f234775837997d9de0a6a030195a21a1
vision-2 <biplovdhakal@gmail.com>
2025-10-14 15:21:23 +0545

    muji

--- Backend/deployment_settings.py
+++ Backend/deployment_settings.py
@@ -3,14 +3,12 @@ import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = ['wearup-backend.onrender.com']
-CSRF_TRUSTED_ORIGINS = ['https://wearup-Backend.onrender.com']
+render_hostname = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+ALLOWED_HOSTS = [host for host in [render_hostname, 'wearup-backend.onrender.com'] if host]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
-ALLOWED_HOSTS = ['wearup-django.onrender.com']
-CSRF_TRUSTED_ORIGINS = ['https://wearup-django.onrender.com']
 
 
 
